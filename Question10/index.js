6e4278024eac86f5ce68a8bdb91654382e5cbc8a
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:16:35 +0500

    Add files via upload

---
+++ Question10/index.js
@@ -0,0 +1,5 @@
+//This is Question number 10th.i have written a single line comment.
+/* In this way i can add multiline comment.
+hope you get me*/
+//My name is Quratulain Faheem.today date is 24-3-2024
+console.log("This question is about adding multiline and single line comment.i used");
