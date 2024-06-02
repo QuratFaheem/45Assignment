dde9a2eb9e2d4cb479e3be28f18b3231eddd3c6f
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:16:07 +0500

    Add files via upload

---
+++ Question9/index.js
@@ -0,0 +1,6 @@
+//Q9: Fovourite Number: Store your fovaourite number in a variable. Then, using that variable, create a message that reveals your favourite number. Print that message.
+var favoriteNumber = 7;
+// Create a message revealing your favorite number
+var msg = "My favorite number is ".concat(favoriteNumber, ".");
+// Print the message
+console.log(msg);
---
+++ Question9/index.ts
@@ -0,0 +1,9 @@
+//Q9: Fovourite Number: Store your fovaourite number in a variable. Then, using that variable, create a message that reveals your favourite number. Print that message.
+
+const favoriteNumber: number = 7;
+
+// Create a message revealing your favorite number
+let msg: string = `My favorite number is ${favoriteNumber}.`;
+
+// Print the message
+console.log(msg);
