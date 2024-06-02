dfdf3ff147dc39eb95d4b9a8e0a4831f1477a7b7
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:15:10 +0500

    Add files via upload

---
+++ Question7/index.ts
@@ -0,0 +1,11 @@
+//This is 7th question
+let num1=4;
+let num2=2;
+let result=num1+num2;
+console.log("The addition of 4 and 2 is "+ result);
+result=num1*num2;
+console.log("The multiplication of 4 and 2 is "+ result);
+result=num1-num2;
+console.log("The substraction os 4 and 2 is "+ result);
+result=num1/num2;
+console.log("The division os 4 and 2 is "+ result);
