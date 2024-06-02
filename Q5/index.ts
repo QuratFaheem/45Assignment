0315a18e28016bccd0af9fdaa319971a79c925e8
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:14:12 +0500

    Add files via upload

---
+++ Q5/index.ts
@@ -0,0 +1,5 @@
+//This is 5th question
+let famous_person="Albert Einstein";
+let message='"A person who never made a mistake never tried anything new."';
+
+console.log(famous_person +" Once said, " + message);
