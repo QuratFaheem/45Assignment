ea5a96779a2236df49adbb906e3966f88cfdefde
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:15:40 +0500

    Add files via upload

---
+++ Question8/index.ts
@@ -0,0 +1,11 @@
+//This is 8th question.Create four lines that look like:
+console.log("Q8: You should create four lines that look like this:");
+
+console.log("____________________");
+console.log(5+3);
+console.log("____________________");
+console.log(10-2);
+console.log("____________________");
+console.log(4*2);
+console.log("____________________");
+console.log(16/2);
