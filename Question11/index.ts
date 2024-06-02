24d612e5e73625ae5d28dd1ee746d378d7034abc
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:17:04 +0500

    Add files via upload

---
+++ Question11/index.ts
@@ -0,0 +1,10 @@
+//Q11: Names: Store the names of your friends in a array called names. Print each person's name by accessing each element in the list, one at a time.
+
+//i declare and filled array
+let friends_list=["Zoya","Sania","Bashair","Taha"];
+
+//now iterating and printing array element one by one using the for lope
+
+for (let num=0;num<friends_list.length;num++)
+    {console.log(friends_list[num]);
+    }
