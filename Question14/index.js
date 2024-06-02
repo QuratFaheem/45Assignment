a6ea27a1113a281456ea199401a6b6cfdd375b8e
QuratFaheem <160398882+QuratFaheem@users.noreply.github.com>
2024-06-02 14:18:28 +0500

    Add files via upload

---
+++ Question14/index.js
@@ -0,0 +1,6 @@
+//Q14: Guest List.Use your list to print a message to each person, inviting to dinner.
+var guest_list = ['Sana', 'Sara', 'Mark'];
+for (var num = 0; num < guest_list.length; num++) {
+    console.log('Dear', guest_list[num], '! \n i cordially invite you on dinner tonight');
+    console.log('\n');
+}
---
+++ Question14/index.ts
@@ -0,0 +1,8 @@
+//Q14: Guest List.Use your list to print a message to each person, inviting to dinner.
+
+let guest_list=['Sana','Sara','Mark'];
+for (let num=0;num<guest_list.length;num++)
+{
+    console.log('Dear', guest_list[num],'! \n i cordially invite you on dinner tonight')
+    console.log('\n');
+}
